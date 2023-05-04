c4c632da5aebdede5fe3aecffca283ad0777bc18
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-05 00:01:28 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- ranked by their longevity (in years).
 -- Output displays only the 'band_name' and 'lifespan' columns.
 
-SELECT band_name, (split-formed) as lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) as lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
