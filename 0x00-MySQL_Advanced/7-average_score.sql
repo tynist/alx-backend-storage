904bc2dfe763f51fd6bcd7e54d41b51d17b4e875
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-05 00:39:20 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,17 +3,14 @@
 -- procedure takes 1 input parameter
 
 drop procedure IF EXISTS ComputeAverageScoreForUser;
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+        IN user_id INT
+)
 BEGIN
-    UPDATE users
-    SET average_score = (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE user_id = users.id
-    )
-    WHERE id = user_id;
-END //
-
+        UPDATE users
+        SET average_score=(SELECT AVG(score) FROM corrections
+                            WHERE corrections.user_id=user_id)
+        WHERE id=user_id;
+END;$$
 DELIMITER ;
