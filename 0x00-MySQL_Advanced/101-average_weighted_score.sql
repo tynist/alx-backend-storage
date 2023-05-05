380a6f2b5268aaf4b2a26aa0469867f4f1f9792b
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-05 02:04:56 +0100

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -2,51 +2,20 @@
 -- that computes and stores the average weighted score for all students
 
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+
 DELIMITER $$
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE user_id INT;
+  UPDATE users AS Us, (
+    SELECT Us.id, SUM(score * weight) / SUM(weight) AS weight_avg 
+    FROM users AS Us
+    JOIN corrections as Cor ON Us.id = Correct.user_id 
+    JOIN projects AS Proj ON Correct.project_id = Proj.id 
+    GROUP BY Us.id
+  ) AS WeighA
+  SET Us.average_score = WeighA.weight_avg 
+  WHERE Us.id = WeighA.id;
+END;
 
-    DECLARE total_weighted_score FLOAT DEFAULT 0;
-    DECLARE total_weight FLOAT DEFAULT 0;
-    DECLARE average_score FLOAT DEFAULT 0;
-
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-
-    OPEN user_cursor;
-
-    user_loop: LOOP
-        FETCH user_cursor INTO user_id;
-        IF done THEN
-            LEAVE user_loop;
-        END IF;
-
-        SET total_weighted_score = 0;
-        SET total_weight = 0;
-
-        SELECT SUM(corrections.score * projects.weight) INTO total_weighted_score
-            FROM corrections
-                INNER JOIN projects
-                    ON corrections.project_id = projects.id
-            WHERE corrections.user_id = user_id;
-
-        SELECT SUM(projects.weight) INTO total_weight
-            FROM corrections
-                INNER JOIN projects
-                    ON corrections.project_id = projects.id
-            WHERE corrections.user_id = user_id;
-
-        IF total_weight = 0 THEN
-            SET average_score = 0;
-        ELSE
-            SET average_score = total_weighted_score / total_weight;
-        END IF;
-
-        UPDATE users SET average_score = average_score WHERE id = user_id;
-    END LOOP;
-
-    CLOSE user_cursor;
-END $$
 DELIMITER ;
