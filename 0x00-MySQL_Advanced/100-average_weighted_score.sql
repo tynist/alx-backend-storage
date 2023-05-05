b5e66d679a9f079f42fed8feca653e79068c89ff
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-05 01:41:26 +0100

    Update 100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -2,22 +2,36 @@
 -- that computes and store the average weighted score for a student.
 -- Procedure ComputeAverageScoreForUser is taking 1 input.
 
-DELIMITER //
+DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_weight INT;
-    DECLARE avg_score FLOAT;
-    
-    SELECT SUM(corrections.score * projects.weight) INTO total_score, SUM(projects.weight) INTO total_weight
-    FROM corrections
-    INNER JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id;
-    
-    SET avg_score = total_score / total_weight;
-    
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END//
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
+
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
+END $$
 
 DELIMITER ;
