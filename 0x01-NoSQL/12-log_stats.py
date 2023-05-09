423d56cbcd1b22c1cd755a556590030d7b85a831
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-09 20:04:29 +0100

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,14 +16,14 @@ def log_stats():
     total = logs_collection.count_docs({})
 
     # Retrieve the count for each HTTP method
-    get = logs_collection.count_docs({"method": "GET"})
-    post = logs_collection.count_docs({"method": "POST"})
-    put = logs_collection.count_docs({"method": "PUT"})
-    patch = logs_collection.count_docs({"method": "PATCH"})
-    delete = logs_collection.count_docs({"method": "DELETE"})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
 
     # Retrieve the count for a specific method and path
-    path = logs_collection.count_docs(
+    path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
 
     # Print the retrieved statistics
