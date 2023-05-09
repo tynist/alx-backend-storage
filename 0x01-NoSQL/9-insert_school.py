fba39f033edf43e7d27b32108cfb4f1eb39e6bf5
Peter Ughasi <ximonaka@gmail.com>
2023-05-09 10:31:52 -0700

    nosql

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,9 +5,11 @@
 def insert_school(mongo_collection, **kwargs):
     """
     inserts a new document in a collection based on kwargs
-    
-    :param mongo_collection: PyMongo collection object representing the collection
-    :param kwargs: keyword arguments representing the fields & values of the new document
+
+    :param mongo_collection: PyMongo collection object representing
+                            the collection
+    :param kwargs: keyword arguments representing the fields & values
+                    of the new document
     :return: _id of the newly inserted document
     """
     new_docs = mongo_collection.insert_one(kwargs)
