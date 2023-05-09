4b638d019a09c694f48dfa4f41cfd0427fb87bae
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-09 18:45:10 +0100

    nosql

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-"""
-Where can I learn Python?
-"""
+"""Where can I learn Python"""
 
 
 def schools_by_topic(mongo_collection, topic):
     """
-     returns the list of school having a specific topic
-
-    :param mongo_collection:
-    :param topic:
-    :return:
+    Returns the list of school having a specific topic
     """
     return mongo_collection.find({"topics": topic})
