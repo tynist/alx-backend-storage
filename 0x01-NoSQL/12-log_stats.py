3b94f407a8eaf1f3196047f8b707c7d1b072e11c
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-09 19:14:41 +0100

    nosql

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,31 +1,23 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""
+Improve 12-log_stats.py by adding the top 10 of the most present IPs
+"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    """Retrieve and print statistics from the 'nginx' logs collection."""
-    # Connect to the MongoDB instance
+    """ the collection nginx of the database logs:
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-
-    # Access the 'nginx' logs collection
     logs_collection = client.logs.nginx
-
-    # Retrieve the total number of logs
     total = logs_collection.count_documents({})
-
-    # Retrieve the count for each HTTP method
     get = logs_collection.count_documents({"method": "GET"})
     post = logs_collection.count_documents({"method": "POST"})
     put = logs_collection.count_documents({"method": "PUT"})
     patch = logs_collection.count_documents({"method": "PATCH"})
     delete = logs_collection.count_documents({"method": "DELETE"})
-
-    # Retrieve the count for a specific method and path
     path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
-
-    # Print the retrieved statistics
     print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
@@ -34,6 +26,16 @@ def log_stats():
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
     print(f"{path} status check")
+    print("IPs:")
+    sorted_ips = logs_collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+         {"$sort": {"count": -1}}])
+    i = 0
+    for s in sorted_ips:
+        if i == 10:
+            break
+        print(f"\t{s.get('_id')}: {s.get('count')}")
+        i += 1
 
 
 if __name__ == "__main__":
