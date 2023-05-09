6fe035d5f4fe9e0fee6c16fb858eeeadfa191179
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-09 18:06:10 +0100

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,12 +3,21 @@
 List all documents in Python
 """
 
+from pymongo.collection import Collection
 
-def list_all(mongo_collection):
-    """
-    lists all documents in a collection
 
-    :param mongo_collection:
-    :return:
+def list_all(mongo_collection: Collection) -> list[dict]:
     """
-    return mongo_collection.find()
+    Retrieves all documents from a MongoDB collection.
+    Args:
+        mongo_collection: A PyMongo collection object representing the collection.
+    Returns:
+        A list of dictionaries representing the documents in the collection.
+    """
+    # Use the find() method to retrieve all documents from the collection
+    cursor = mongo_collection.find()
+
+    # Convert the cursor to a list of dictionaries
+    documents = list(cursor)
+
+    return documents
