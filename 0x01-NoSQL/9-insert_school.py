a113f64827d8785e612941f79fadbde427bf158e
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-09 20:49:51 +0100

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -12,5 +12,5 @@ def insert_school(mongo_collection, **kwargs):
     of the new document
     :return: _id of the newly inserted document
     """
-    new_documents = mongo_collection.insert_one(kwargs)
-    return new_documents.inserted_id
+    new_docs = mongo_collection.insert_one(kwargs)
+    return new_docs.inserted_id
