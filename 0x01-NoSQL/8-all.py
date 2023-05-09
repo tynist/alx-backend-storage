ddf6d999b7789c1471125f85a5503640d966bd1a
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-09 18:18:36 +0100

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,7 +5,8 @@
 def list_all(mongo_collection):
     """
     lists all documents in a collection
-    :param mongo_collection: A PyMongo collection object representing the collection.
-    :return: A cursor pointing to the result of the find operation.
+    
+    :param mongo_collection: A PyMongo collection object representing the collection
+    :return: A cursor containing the result of the find operation
     """
     return mongo_collection.find()
