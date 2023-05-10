ef2d07100f8132df4bc7987a464e5bccaa15da29
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-10 18:06:58 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
+from functools import wraps
 
 UnionOfTypes = Union[str, bytes, int, float]
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of times a method is called.
+    Increments the count for the method's qualified name in Redis.
+    Returns the value returned by the original method.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    
+    return wrapper
+
 class Cache:
     """
-    Cache class that stores data in Redis.
+    Cache class that stores data in Redis and provides retrieval methods.
     """
 
     def __init__(self):
@@ -27,13 +42,49 @@ class Cache:
 
         Returns:
             The randomly generated key used to store the data.
-
-        Example:
-            cache = Cache()
-            data = b"hello"
-            key = cache.store(data)
-            print(key)
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
+        """
+        Retrieves the stored data from Redis and optionally applies a conversion function.
+
+        Args:
+            key: The key used to retrieve the data from Redis.
+            fn: Optional conversion function to apply to the retrieved data.
+
+        Returns:
+            The retrieved data, optionally converted based on the provided conversion function.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieves a UTF-8 string from Redis.
+
+        Args:
+            key: The key used to retrieve the data from Redis.
+
+        Returns:
+            The retrieved data as a UTF-8 string.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieves an integer from Redis.
+
+        Args:
+            key: The key used to retrieve the data from Redis.
+
+        Returns:
+            The retrieved data as an integer.
+        """
+        return self.get(key, fn=int)
