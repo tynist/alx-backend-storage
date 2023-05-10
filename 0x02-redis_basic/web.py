f21279fa5e54ce8ef55e84c8d0565fb00c9136f8
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-10 23:38:23 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Implementing an expiring web cache and tracker
+"""
 import requests
 import redis
 import time
@@ -7,35 +10,47 @@ from functools import wraps
 # Redis client
 redis_client = redis.Redis()
 
-def count_calls(method):
-    """
-    Decorator that tracks the number of times a function is called with a specific URL.
-    Uses Redis to store and retrieve the count.
-    """
-    @wraps(method)
-    def wrapper(url):
-        count_key = f"count:{url}"
-        count = redis_client.get(count_key)
-        if count:
-            count = int(count)
-            count += 1
-        else:
-            count = 1
-        redis_client.setex(count_key, 10, str(count))
-        return method(url)
 
+def wrap_requests(fn: Callable) -> Callable:
+    """
+    Decorator wrapper that tracks URL access count and caches results.
+    """
+
+    @wraps(fn)
+    def wrapper(url):
+        """
+        Wrapper function for the decorator.
+        Increments URL access count and checks for cached result.
+        If cached result is available, returns it.
+        Otherwise, calls the original function, caches the result, and returns it.
+        """
+        # Increment the count for the URL
+        redis_client.incr(f"count:{url}")
+        
+        # Check if the result is already cached
+        cached_response = redis_client.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        
+        # Call the original function to get the result
+        result = fn(url)
+        
+        # Cache the result with an expiration time of 10 seconds
+        redis_client.setex(f"cached:{url}", 10, result)
+        return result
     return wrapper
 
 
-def get_page(url):
+@wrap_requests
+def get_page(url: str) -> str:
     """
-    Retrieves the HTML content of a given URL.
+    Fetches the HTML content of a URL using the requests module.
 
     Args:
-        url: The URL of the page to retrieve.
+        url: The URL to fetch.
 
     Returns:
-        The HTML content of the page.
+        The HTML content of the URL as a string.
     """
     response = requests.get(url)
     return response.text
