8d55f61dde75b013daa6411be8a8a37335dd8533
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-11 00:35:49 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,47 +1,42 @@
 #!/usr/bin/env python3
-"""
-Redis module
-"""
-import sys
-from functools import wraps
+import redis
 from typing import Union, Optional, Callable
 from uuid import uuid4
-
-import redis
+import sys
+from functools import wraps
 
 UnionOfTypes = Union[str, bytes, int, float]
 
 
 def count_calls(method: Callable) -> Callable:
     """
-    a system to count how many
-    times methods of the Cache class are called.
-    :param method:
-    :return:
+    Decorator that counts the number of times a method is called.
+
+    Args"
+        method: The method being decorated.
+
+    Returns:
+        The wrapped method.
     """
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """
-        Wrap
-        :param self:
-        :param args:
-        :param kwargs:
-        :return:
-        """
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-
     return wrapper
 
 
 def call_history(method: Callable) -> Callable:
     """
-    add its input parameters to one list
-    in redis, and store its output into another list.
-    :param method:
-    :return:
+    Decorator that adds the input parameters of a method to a
+    list in Redis and stores its output into another list.
+
+    Args:
+        method: The method being decorated.
+
+    Returns:
+        The wrapped method.
     """
     key = method.__qualname__
     i = "".join([key, ":inputs"])
@@ -49,7 +44,7 @@ def call_history(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ Wrapp """
+        """ Wrapper for the decorated method """
         self._redis.rpush(i, str(args))
         res = method(self, *args, **kwargs)
         self._redis.rpush(o, str(res))
@@ -60,12 +55,13 @@ def call_history(method: Callable) -> Callable:
 
 class Cache:
     """
-    Cache redis class
+    Cache class that stores data in Redis & provides retrieval methods
     """
+
 
     def __init__(self):
         """
-        constructor of the redis model
+        Initializes the Redis client and flushes the database
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -74,11 +70,13 @@ class Cache:
     @call_history
     def store(self, data: UnionOfTypes) -> str:
         """
-        generate a random key (e.g. using uuid),
-         store the input data in Redis using the
-          random key and return the key.
-        :param data:
-        :return:
+        Stores the input data in Redis with a randomly generated key
+
+        Args:
+            data to be stored. Can be of type str, bytes, int, or float
+
+        Returns:
+            The randomly generated key used to store the data
         """
         key = str(uuid4())
         self._redis.mset({key: data})
@@ -87,11 +85,14 @@ class Cache:
     def get(self, key: str, fn: Optional[Callable] = None) \
             -> UnionOfTypes:
         """
-        convert the data back
-        to the desired format
-        :param key:
-        :param fn:
-        :return:
+        Retrieves the stored data from Redis & may apply a conversion function.
+
+        Args:
+            key: The key used to retrieve the data from Redis.
+            fn: Optional conversion function to apply to the retrieved data.
+
+        Returns:
+            The retrieved data, may be converted based on d given conversion func
         """
         if fn:
             return fn(self._redis.get(key))
@@ -103,5 +104,12 @@ class Cache:
         return int.from_bytes(self, sys.byteorder)
 
     def get_str(self: bytes) -> str:
-        """get a string"""
+        """Retrieves a UTF-8 string from Redis.
+
+        Args:
+            key: The key used to retrieve the data from Redis.
+
+        Returns:
+            The retrieved data as a UTF-8 string.
+        """
         return self.decode("utf-8")
